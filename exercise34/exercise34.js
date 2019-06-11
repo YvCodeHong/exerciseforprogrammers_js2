32924608f12d5f021428b49ed607a3ca97e932f2
Yvonne Tang <ytang604@gmail.com>
2019-06-12 00:09:51 +0100

    complete exercise34

--- exercise34/exercise34.js
+++ exercise34/exercise34.js
@@ -4,4 +4,41 @@
 //that specific name from the list of names. Display the 
 //remainiing employees, each on its own line 
 
+//e.g.: 
+// There are 5 employees: 
+// Joan Smith
+//Jackie Jackson
+//Chris Jones
+//Amanda Cullen
+// Jeremy Goodwin
+
+// Enter an employee name to remove: Chris Jones
+// There are 4 employees 
+// Joan Smith
+// Jackie Jackson
+//Amanda Cullen
+//Jeremy Goodwin. 
+
+function listRemoval() {
+    const employeeList = ["Joan Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"];
+    const listofNames  = alert("There are" + " " + employeeList.length + " " + "employees" + ":" + " " + employeeList);
+    const employeeName = prompt("Enter an employee name to remove"); 
+    const newemployeeList = employeeList.indexOf(employeeName);
+        if(newemployeeList >- 1) {
+            employeeList.splice(newemployeeList, 1);
+        }
+    return alert("There are" + " " + employeeList.length + " " + employeeList)
+}
+
+
+
+
+
+
+     
+
+
+
+
+
 

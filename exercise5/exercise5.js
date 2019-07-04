7aa507f186838d066b6ba6b55bdf946041a89965
Yvonne Tang <ytang604@gmail.com>
2019-07-04 11:01:29 +0100

    add feature-user are not allowed to enter negative number

--- exercise5/exercise5.js
+++ exercise5/exercise5.js
@@ -6,6 +6,10 @@
 function SimpleMath() {
     var number = +prompt("What is the first number?");
     var secondNumber = +prompt("What is the second number?"); 
+    if(number < 0 || secondNumber < 0) {
+        alert("Negative Value is not allowed");
+        return false;
+    }
     alert(number + secondNumber);
     alert(number - secondNumber);
     alert(number * secondNumber);
@@ -14,4 +18,5 @@ function SimpleMath() {
 
 console.log(SimpleMath());
 
-  
+//Revise the program to ensure that inputs are entered as numeric values.
+//Don't allow the user to proceed if the value entered is n

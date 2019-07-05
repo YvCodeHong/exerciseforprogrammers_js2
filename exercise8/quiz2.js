36946dd60263d2dee63893300800609922871540
Yvonne Tang <ytang604@gmail.com>
2019-07-05 12:53:11 +0100

    complete quiz2

---
+++ exercise8/quiz2.js
@@ -0,0 +1,9 @@
+//Create a variant of the program that prompts for the 
+//number of people and the number of pieces each person
+// wants, and calculate how many full pizzas  you need to 
+//purchase. 
+
+function pizzaParty(person, pieces) {
+    return person * pieces; 
+}
+

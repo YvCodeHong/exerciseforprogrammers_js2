961f1b4b5bd6c0de441d358c2a8886f152f775bd
Yvonne Tang <ytang604@gmail.com>
2019-07-07 17:57:41 +0100

    add exercise11

---
+++ Calculations/exercise11/exercise11.js
@@ -0,0 +1,17 @@
+//Currency Conversion 
+
+//Write a program that converts currency. Specifically, convert
+//euros to U.S dollars. Prompt for the amount of money in 
+//euros you have, and prompt for the current exchange rate
+//of the euros. Print out the new amount in U.S dollars. The 
+//formula for currency conversion is 
+
+
+function currencyConversion() {
+    const euros = prompt("How many euros are you exchanging?");
+    const exchangeRate = prompt("What is the exchange rate?");
+    const amount = euros * (exchangeRate/100);
+    const twoDecimal = amount.toFixed(2);
+    alert(euros +  ' ' + "euros at an exchange rate of" +  ' ' + exchangeRate + ' ' + "is" + ' ' + twoDecimal + ' ' + "U.S. dollars");
+}
+

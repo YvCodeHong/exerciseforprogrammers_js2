3308c50001e193d274ef12ac32e081789c2ad4b4
Yvonne Tang <ytang604@gmail.com>
2019-07-08 17:01:38 +0100

    add exercise15

---
+++ Making_Decisions/exercise16/exercise16.js
@@ -0,0 +1,14 @@
+// Write a program that asks the user for their age and compare
+//it to the legal driving age of sixteen. IF the user is sixteen or 
+//order, then then program should display "You are old enough to legally drive"
+// If the user is under sixteen, the program should display "You are not old enough to legally drive". 
+
+function legaldrivingAge() {
+    const age = prompt("What is your age?");
+    const confirmdrivingAge = 16; 
+    if(age < 16) {
+        alert("You are not old enough to legally drive")}
+    else {
+        alert("You are old enough to legally drive");
+        }
+     }

c19392698c8538e1d40cdc7e062120a6c353a0aa
Yvonne Tang <ytang604@gmail.com>
2019-07-08 12:11:08 +0100

    add exercise14

---
+++ Making_Decisions/exercise14/exercise14.js
@@ -0,0 +1,19 @@
+// Tax Calculator 
+//  Write a simple program to compute the tax on an order
+// amount. The program should prompt for the order amount
+// and the state. If the state is "WI," then the order must be 
+//charged 5/5% tax. The program should display the subtotal, 
+//tax, and total for Wisconsin residents but display just the 
+//total for non-residents. 
+
+function taxCalculator() {
+    const order = prompt("What is the order amount?");
+    const state = prompt("What is the state?");
+    const tax = 0.55;
+    const subtotal = order + tax;
+    if (state === "WI") {
+        alert("The total is $" + subtotal);
+    } else {
+        alert("The total is $" + order);
+    }
+}

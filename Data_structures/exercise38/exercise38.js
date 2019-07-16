186f8d1d1247ee33ad0bea48480e0beb2d2b3f8f
Yvonne Tang <ytang604@gmail.com>
2019-07-16 21:20:59 +0100

    add exercise38

---
+++ Data_structures/exercise38/exercise38.js
@@ -0,0 +1,15 @@
+//Filtering Values 
+
+//Create a program that prompts for a list of numbers, 
+//separated by spaces. Have the program print out a new list 
+//constaining only the even numbers. 
+
+function filteringValues() {
+    const num = prompt("Enter a list of numbers, separated by spaces:"); 
+    const listofNum = num.split(" ");
+    for( let i =1; i<listofNum; i++) {
+        if(i % 2 === 0) {
+            console.log("The even numbers are:" + i);
+        }
+    }
+}

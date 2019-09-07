fa63b2601c5d991cb8d5980fe954b3dca9ff4f3a
Yvonne Tang <ytang604@gmail.com>
2019-09-07 19:04:05 +0100

    add nextNumber

---
+++ simpleexercise/nextNumber.js
@@ -0,0 +1,8 @@
+//Create a function that takes a number as an argument, increments the number by +1 and 
+//returns the result 
+
+function nextNumber(num) {
+    for(let i = 0; i < 10; i++) {
+        alert(i);
+    }
+}

2bb094697636d9ff390659556ebab1165e4fd366
Yvonne Tang <ytang604@gmail.com>
2019-09-09 17:41:34 +0100

    add integerdivisible

---
+++ simpleexercise/integerdivisibleFive.js
@@ -0,0 +1,12 @@
+//Check if an Integer is Divisible by Five. 
+//Create a function that returns true if an integer is divisible by 5, 
+//and false otherwise. 
+
+function divisibleFive(num) {
+     if(num % 5 === 0) {
+        return true; 
+     } else {
+        return false;
+     }
+}
+

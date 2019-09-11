f2f861f7b72779bda3d3c2ff1db6b8b49276d61c
Yvonne Tang <ytang604@gmail.com>
2019-09-11 17:48:52 +0100

    add string Empty

---
+++ simpleexercise/stringEmpty.js
@@ -0,0 +1,9 @@
+//Create a function that returns true if a string is empty and false otherwise. 
+
+function stringEmpty(string) {
+    if(string === "") {
+        return true; 
+     } else {
+         return false; 
+     }
+}

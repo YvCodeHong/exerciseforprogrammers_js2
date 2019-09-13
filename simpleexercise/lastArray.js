e5c7c226eebf6982798217d9af5481bf98caff85
Yvonne Tang <ytang604@gmail.com>
2019-09-13 11:51:35 +0100

    add last element array

---
+++ simpleexercise/lastArray.js
@@ -0,0 +1,8 @@
+//Return the Last Element in an Array
+//Create a function that accepts an array and returns the last item in the array. 
+
+
+function lastArray(arr) {
+    let last = arr.pop();
+    return arr;
+}
